904bf068f897ebd2c1d4a8975febd8627241ea86
arnobtarafder <rmtarnob@gmail.com>
2022-04-10 15:31:39 +0600

    regular test

---
+++ src/_auth_steps.js
@@ -0,0 +1,14 @@
+/**
+ * 1. create a project at firebase 
+ * 2. npm install firebase
+ * 3. create firebase.init.js and import getAuth to export auth
+ * 4. Firebase settings > Authentication > enable Email and Password auth
+ */
+
+/**
+ * Hosting Steps
+ * 1. npm install -g firebase-tools
+ * 2. firebase login
+ * 3. firebase init
+ * 4. firebase deploy
+*/
---
+++ src/firebase.init.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBXgGgiFjpfpm430s4Kfk2R95t8xwAz5Aw",
+  authDomain: "ema-john-package.firebaseapp.com",
+  projectId: "ema-john-package",
+  storageBucket: "ema-john-package.appspot.com",
+  messagingSenderId: "178169298206",
+  appId: "1:178169298206:web:fdc039144becc351aadd0f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export default auth;
