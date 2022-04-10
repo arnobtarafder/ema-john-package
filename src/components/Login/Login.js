8dbb2f172e4a34a1ef1f1cc31686ced253a932f8
arnobtarafder <rmtarnob@gmail.com>
2022-04-10 19:28:53 +0600

    implemented login and redirect to shop

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,21 +1,55 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import './Login.css'
 
 const Login = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const navigate = useNavigate()
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth)
+
+    const handleEmailBlur = event => {
+        setEmail(event.target.value)
+    }
+    
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value)
+    }
+
+    if(user) {
+        navigate("/shop")
+    }
+
+    const handleUserLogin = (event) => {
+        event.preventDefault();
+        signInWithEmailAndPassword(email, password)
+    }
+
     return (
         <div className='form-container'>
           <div>
             <h2 className="form-title"> Login </h2>
-            <form action="">
+            <form action="" onSubmit={handleUserLogin}>
              <div className="input-group">
                 <label htmlFor="email">Email</label>
-                <input type="email" name='email' required/>
+                <input onBlur={handleEmailBlur} type="email" name='email' required/>
             </div>
             <div className="input-group">
                 <label htmlFor="password">Password</label>
-                <input type="password" name="password" id="" required/>
+                <input onBlur={handlePasswordBlur} type="password" name="password" id="" required/>
             </div>
+            <p style={{color: "red"}}>{error?.message}</p>
+            {
+                loading && <p>Loading...</p>
+            }
+
             <input type="submit" value="submit" className='form-submit' required/>
             </form>
             <p className="form-suggestion">
